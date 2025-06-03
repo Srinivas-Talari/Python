01d1d18e7c615032dbc0ab5e24dfae7a2f6ed0a2
Srinivas Talari <seenutalari789@gmail.com>
2025-06-03 11:30:53 +0530

    added about author

--- intro.py
+++ intro.py
@@ -1,3 +1,17 @@
+# About Python
+
+'''Python is a programming language which is used to communicate with computers to get desired results that a machine can do which are soo accurate than manual results'''
+
+'''Discovered by Van Guido Rossum in 1991 and it is named after his  favorite show called "Monty Python Flying Circus"'''
+
+#Why python
+
+'''
+1.Easy to read and write because of simple syntax
+2.Cross Platform( windows, linux, etc)
+3.Used in multiple fields(Web Dev,AI/ML)
+'''
+
 # PIP
 
 """ It's like a adding functionality to our codes, by using internal or external libraries..So to install Libraries we need a Package Manager called "PIP".(Just as "npm" for Javascript) """
