387d62741fc5e1844012fa82e07248185f4aff2c
Srinivas Talari <seenutalari789@gmail.com>
2025-06-03 11:23:08 +0530

    add intro file

---
+++ intro.py
@@ -0,0 +1,17 @@
+# PIP
+
+""" It's like a adding functionality to our codes, by using internal or external libraries..So to install Libraries we need a Package Manager called "PIP".(Just as "npm" for Javascript) """
+
+# Modules
+
+"""If we want import or export files we go with modules"""
+
+#Comments
+
+#Single Line Comment
+
+'''Double Line Comment'''
+
+#Press 'python' in terminal and make it use as a calculator 
+
+
